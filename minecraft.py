e3ab35c89d0b63552cd064f447140e12e3177dbe
David Walker <d0sboots@gmail.com>
2010-10-29 22:17:56 -0700

    Big pile o' workage!

--- minecraft.py
+++ minecraft.py
@@ -14,76 +14,199 @@ TAG_STRING = 8
 TAG_LIST = 9
 TAG_COMPOUND = 10
 
+_tag_names = ["TAG_End", "TAG_Byte", "TAG_Short", "TAG_Int",
+              "TAG_Long", "TAG_Float", "TAG_Double", "TAG_Byte_Array",
+              "TAG_String", "TAG_List", "TAG_Compound"]
+
+_numeric_conversions = {
+    TAG_BYTE: (">b", 1), TAG_SHORT: (">h", 2),
+    TAG_INT: (">i", 4), TAG_LONG: (">q", 8),
+    TAG_FLOAT: (">f", 4), TAG_DOUBLE: (">d", 8)}
+
 def _parse_named_tag(file_):
-    """Returns a named tag as a (type, name, value) tuple."""
+    """Returns a named tag as a (value, type, name) tuple."""
     type_ = ord(file_.read(1))
+    if type_ == TAG_END:
+        # Early out because the other fields won't exist.
+        return (None, type_, None)
     name = _parse_tag(TAG_STRING, file_)
     value = _parse_tag(type_, file_)
-    return (type_, name, value)
+    return (value, type_, name)
+
+def _write_named_tag(value, type_, name, file_):
+    file_.write(chr(type_))
+    _write_tag(name, TAG_STRING, file_)
+    _write_tag(value, type_, file_)
 
 def _parse_tag(tag_id, file_):
     """Helper to parse the actual data."""
     if tag_id == TAG_END:
         raise ValueError("Unexpected TAG_END")
     elif tag_id >= TAG_BYTE and tag_id <= TAG_DOUBLE:
-        return _parse_numeric(tag_id, file_)
+        conversion = _numeric_conversions[tag_id]
+        bytes = file_.read(conversion[1])
+        return unpack(conversion[0], bytes)[0]
     elif tag_id == TAG_BYTE_ARRAY:
-        pass
+        length = _parse_tag(TAG_INT, file_)
+        return file_.read(length)
     elif tag_id == TAG_STRING:
-        pass
+        length = _parse_tag(TAG_SHORT, file_)
+        return file_.read(length).decode("UTF-8")
     elif tag_id == TAG_LIST:
-        pass
+        return NBTList.from_file(file_)
     elif tag_id == TAG_COMPOUND:
-        return _parse_compound(file_)
+        return NBTCompound.from_file(file_)
     else:
-        raise ValueError("Unknown tag type %d" % type_)
+        raise ValueError("Unknown tag type %d" % tag_id)
 
-_numeric_conversions = {
-    }
+def _write_tag(value, tag_id, file_):
+    """Helper to write the actual data."""
+    if tag_id == TAG_END:
+        raise ValueError("Unexpected TAG_END")
+    elif tag_id >= TAG_BYTE and tag_id <= TAG_DOUBLE:
+        conversion = _numeric_conversions[tag_id]
+        file_.write(pack(conversion[0], value))
+    elif tag_id == TAG_BYTE_ARRAY:
+        _write_tag(len(value), TAG_INT, file_)
+        file_.write(value)
+    elif tag_id == TAG_STRING:
+        string = value.encode("UTF-8")
+        _write_tag(len(string), TAG_SHORT, file_)
+        file_.write(string)
+    elif tag_id == TAG_LIST:
+        return NBTList.to_file(value, file_)
+    elif tag_id == TAG_COMPOUND:
+        return NBTCompound.to_file(value, file_)
+    else:
+        raise ValueError("Unknown tag type %d" % tag_id)
 
-def _parse_numeric(tag_id, file_):
-    pass
+def _guess_type(value):
+    """Tries to guess the tag type of a python data type."""
+    if isinstance(value, int):
+        return TAG_INT
+    elif isinstance(value, long):
+        return TAG_LONG
+    elif isinstance(value, float):
+        return TAG_DOUBLE
+    elif isinstance(value, basestring):
+        return TAG_STRING
+    elif isinstance(value, NBTList):
+        return TAG_LIST
+    elif isinstance(value, NBTCompound):
+        return TAG_COMPOUND
+    else:
+        raise ValueError("Can't figure out a type for %r", value)
 
-def _parse_compound(file_):
-    return NBT()
 
-class NBT(dict):
+class NBTCompound(dict):
     """Represents an NBT Compound object.
 
-    Rather than having a class per tag type, we keep all the logic in the NBT
-    class, and use native python types for the leaf values.
+    Rather than having a class per tag type, we only wrap dicts (NBTCompound)
+    and lists (NBTList), and use native python types for the leaf values.
+
+    Every memeber has a type value that is used for serialization. If the type
+    is not specified, one will be guessed based on the native python type:
+    INT, LONG, DOUBLE, or STRING as appropriate.
     """
-    _tag_names = ["TAG_End", "TAG_Byte", "TAG_Short", "TAG_Int",
-                  "TAG_Long", "TAG_Float", "TAG_Double", "TAG_Byte_Array",
-                  "TAG_String", "TAG_List", "TAG_Compound"]
+
+    def __init__(self, *args, **kwargs):
+        super(NBTCompound, self).__init__(*args, **kwargs)
+        self.types = {}
+
     @classmethod
     def from_file(cls, file_):
-        """Returns a parsed NBT from a file-like object."""
-        type_, name, nbt = _parse_named_tag(file_)
+        nbt = NBTCompound()
+        while True:
+            value, tag_id, name = _parse_named_tag(file_)
+            if tag_id == TAG_END:
+                return nbt
+            nbt[name] = value
+            nbt.types[name] = tag_id
+
+    def to_file(self, file_):
+        for name, value in self.iteritems():
+            type_ = self.types.setdefault(name, _guess_type(value))
+            _write_named_tag(value, type_, name, file_)
+        file_.write('\0')
+
+class NBTFile(NBTCompound):
+    """The top-level object that represents a parsed NBT file.
+
+    This differs from NBTCompound only in that it has a name, and additional
+    methods.
+    """
+    def __init__(self, *args, **kwargs):
+        super(NBTFile, self).__init__(*args, **kwargs)
+        self.name = ""
+
+    @classmethod
+    def from_file(cls, file_):
+        """Returns a parsed Compound from a file-like object."""
+        compound, type_, name = _parse_named_tag(file_)
         if type_ != TAG_COMPOUND:
             raise ValueError("Top-level tag of type %d, expected %d" %
                              (type_, TAG_COMPOUND))
+        nbt = NBTFile(compound)
         nbt.name = name
         return nbt
 
     @classmethod
     def from_string(cls, string):
-        """Returns a parsed NBT from a string."""
+        """Returns a parsed Compound from a string."""
         return cls.from_file(StringIO(string))
 
     @classmethod
     def from_filename(cls, filename):
-        """Returns a parsed NBT from a NBT file with the given filename."""
+        """Returns a parsed Compound from a NBT file with the given filename."""
         return cls.from_file(GzipFile(filename))
 
     def to_file(self, file_):
-        pass
+        """Serialize this compound to a file-like stream."""
+        _write_named_tag(self, TAG_COMPOUND, self.name, file_)
 
     def to_string(self):
+        """Serialize this compound as a string."""
         io = StringIO()
         self.to_file(io)
         return io.getvalue()
 
     def to_filename(self, filename):
+        """Serialize to an NBT file."""
         file_ = GzipFile(filename, "wb")
         self.to_file(file_)
+
+
+class NBTList(list):
+    """Represents an NBT List object.
+
+    This is wrapped in a class because the type_ property specifies the type
+    across all the items. If the type is unspecified it will be guessed during
+    serialization based on the first member."""
+
+    def __init__(self, *args, **kwargs):
+        list.__init__(self, *args, **kwargs)
+        self.type_ = None
+
+    @classmethod
+    def from_file(cls, file_):
+        """Returns a parsed list from a file-like object."""
+
+        list_ = NBTList()
+        list_.type_ = _parse_tag(TAG_BYTE, file_)
+        length = _parse_tag(TAG_INT, file_)
+        for unused in range(length):
+            list_.append(_parse_tag(list_.type_, file_))
+        return list_
+
+    def to_file(self, file_):
+        """Serializes the list to a file-like object."""
+        if self.type_ is None:
+            if len(self):
+                self.type_ = _guess_type(self[0])
+            else:
+                # Assume INT for lack of anything smarter.
+                self.type_ = TAG_INT
+        _write_tag(self.type_, TAG_BYTE, file_)
+        _write_tag(len(self), TAG_INT, file_)
+        for item in self:
+            _write_tag(item, self.type_, file_)
--- test/test_nbt.py
+++ test/test_nbt.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-from minecraft import NBT
+from minecraft import NBTFile
 import unittest
 from StringIO import StringIO
 from gzip import GzipFile
 
 class SmokeTest(unittest.TestCase):
     def setUp(self):
-        self.nbt = NBT.from_filename("bigtest.nbt")
+        self.nbt = NBTFile.from_filename("bigtest.nbt")
 
     def testReadBig(self):
         # Test basic parsing works.
@@ -16,7 +16,7 @@ class SmokeTest(unittest.TestCase):
 
     def testWriteBig(self):
         output = self.nbt.to_string()
-        newnbt = NBT.from_string(output)
+        newnbt = NBTFile.from_string(output)
         self.assertEqual(self.nbt, newnbt)
 
     def tearDown(self):
@@ -26,7 +26,7 @@ class SmokeTest(unittest.TestCase):
 class EmptyStringTest(unittest.TestCase):
     def setUp(self):
         self.golden_value = "\x0A\0\x04Test\x08\0\x0Cempty string\0\0\0"
-        self.nbt = NBT.from_string(self.golden_value)
+        self.nbt = NBTFile.from_string(self.golden_value)
 
     def testReadEmptyString(self):
         self.assertEqual(self.nbt.name, "Test")
