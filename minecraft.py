7b8c008f82d8b5fe74493ca848895c1d19747022
David Walker <d0sboots@gmail.com>
2010-10-29 19:32:34 -0700

    Retab and more WIP.

--- minecraft.py
+++ minecraft.py
@@ -15,54 +15,75 @@ TAG_LIST = 9
 TAG_COMPOUND = 10
 
 def _parse_named_tag(file_):
-  """Returns a named tag as a (type, name, value) tuple."""
-  type_ = ord(file_.read(1))
-  if type_ > TAG_COMPOUND:
-    raise ValueError("Unknown tag type %d" % type_)
-  name = _parse_tag(TAG_STRING, file_)
-  value = _parse_tag(type_, file_)
-  return (type_, name, value)
+    """Returns a named tag as a (type, name, value) tuple."""
+    type_ = ord(file_.read(1))
+    name = _parse_tag(TAG_STRING, file_)
+    value = _parse_tag(type_, file_)
+    return (type_, name, value)
 
 def _parse_tag(tag_id, file_):
-  """Helper to parse the actual data."""
+    """Helper to parse the actual data."""
+    if tag_id == TAG_END:
+        raise ValueError("Unexpected TAG_END")
+    elif tag_id >= TAG_BYTE and tag_id <= TAG_DOUBLE:
+        return _parse_numeric(tag_id, file_)
+    elif tag_id == TAG_BYTE_ARRAY:
+        pass
+    elif tag_id == TAG_STRING:
+        pass
+    elif tag_id == TAG_LIST:
+        pass
+    elif tag_id == TAG_COMPOUND:
+        return _parse_compound(file_)
+    else:
+        raise ValueError("Unknown tag type %d" % type_)
 
-class NBT(dict):
-  """Represents an NBT Compound object.
+_numeric_conversions = {
+    }
 
-  Rather than having a class per tag type, we keep all the logic in the
-  NBT class, and use native python types for the leaf values.
-  """
-  _tag_names = ["TAG_End", "TAG_Byte", "TAG_Short", "TAG_Int",
-                "TAG_Long", "TAG_Float", "TAG_Double", "TAG_Byte_Array",
-                "TAG_String", "TAG_List", "TAG_Compound"]
-  @classmethod
-  def from_file(cls, file_):
-    """Returns a parsed NBT from a file-like object."""
-    type_, name, nbt = _parse_named_tag(file_)
-    if type_ != TAG_COMPOUND:
-      raise ValueError("Top-level tag of type %d, expected %d" %
-                       (type_, TAG_COMPOUND))
-    nbt.name = name
-    return nbt
-
-  @classmethod
-  def from_string(cls, string):
-    """Returns a parsed NBT from a string."""
-    return cls.from_file(StringIO(string))
-
-  @classmethod
-  def from_filename(cls, filename):
-    """Returns a parsed NBT from a NBT file with the given filename."""
-    return cls.from_file(GzipFile(filename))
-
-  def to_file(self, file_):
+def _parse_numeric(tag_id, file_):
     pass
 
-  def to_string(self):
-    io = StringIO()
-    self.to_file(io)
-    return io.getvalue()
+def _parse_compound(file_):
+    return NBT()
 
-  def to_filename(self, filename):
-    file_ = GzipFile(filename, "wb")
-    self.to_file(file_)
+class NBT(dict):
+    """Represents an NBT Compound object.
+
+    Rather than having a class per tag type, we keep all the logic in the NBT
+    class, and use native python types for the leaf values.
+    """
+    _tag_names = ["TAG_End", "TAG_Byte", "TAG_Short", "TAG_Int",
+                  "TAG_Long", "TAG_Float", "TAG_Double", "TAG_Byte_Array",
+                  "TAG_String", "TAG_List", "TAG_Compound"]
+    @classmethod
+    def from_file(cls, file_):
+        """Returns a parsed NBT from a file-like object."""
+        type_, name, nbt = _parse_named_tag(file_)
+        if type_ != TAG_COMPOUND:
+            raise ValueError("Top-level tag of type %d, expected %d" %
+                             (type_, TAG_COMPOUND))
+        nbt.name = name
+        return nbt
+
+    @classmethod
+    def from_string(cls, string):
+        """Returns a parsed NBT from a string."""
+        return cls.from_file(StringIO(string))
+
+    @classmethod
+    def from_filename(cls, filename):
+        """Returns a parsed NBT from a NBT file with the given filename."""
+        return cls.from_file(GzipFile(filename))
+
+    def to_file(self, file_):
+        pass
+
+    def to_string(self):
+        io = StringIO()
+        self.to_file(io)
+        return io.getvalue()
+
+    def to_filename(self, filename):
+        file_ = GzipFile(filename, "wb")
+        self.to_file(file_)
