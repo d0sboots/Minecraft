dd3f77e9737b3932d7c41f937864936812fde1f0
David Walker <d0sboots@gmail.com>
2010-10-29 19:01:10 -0700

    Basic helper methods, no impl yet.

--- minecraft.py
+++ minecraft.py
@@ -1,4 +1,6 @@
 from struct import pack, unpack
+from gzip import GzipFile
+from cStringIO import StringIO
 
 TAG_END = 0
 TAG_BYTE = 1
@@ -12,9 +14,55 @@ TAG_STRING = 8
 TAG_LIST = 9
 TAG_COMPOUND = 10
 
+def _parse_named_tag(file_):
+  """Returns a named tag as a (type, name, value) tuple."""
+  type_ = ord(file_.read(1))
+  if type_ > TAG_COMPOUND:
+    raise ValueError("Unknown tag type %d" % type_)
+  name = _parse_tag(TAG_STRING, file_)
+  value = _parse_tag(type_, file_)
+  return (type_, name, value)
+
+def _parse_tag(tag_id, file_):
+  """Helper to parse the actual data."""
+
 class NBT(dict):
   """Represents an NBT Compound object.
 
   Rather than having a class per tag type, we keep all the logic in the
   NBT class, and use native python types for the leaf values.
   """
+  _tag_names = ["TAG_End", "TAG_Byte", "TAG_Short", "TAG_Int",
+                "TAG_Long", "TAG_Float", "TAG_Double", "TAG_Byte_Array",
+                "TAG_String", "TAG_List", "TAG_Compound"]
+  @classmethod
+  def from_file(cls, file_):
+    """Returns a parsed NBT from a file-like object."""
+    type_, name, nbt = _parse_named_tag(file_)
+    if type_ != TAG_COMPOUND:
+      raise ValueError("Top-level tag of type %d, expected %d" %
+                       (type_, TAG_COMPOUND))
+    nbt.name = name
+    return nbt
+
+  @classmethod
+  def from_string(cls, string):
+    """Returns a parsed NBT from a string."""
+    return cls.from_file(StringIO(string))
+
+  @classmethod
+  def from_filename(cls, filename):
+    """Returns a parsed NBT from a NBT file with the given filename."""
+    return cls.from_file(GzipFile(filename))
+
+  def to_file(self, file_):
+    pass
+
+  def to_string(self):
+    io = StringIO()
+    self.to_file(io)
+    return io.getvalue()
+
+  def to_filename(self, filename):
+    file_ = GzipFile(filename, "wb")
+    self.to_file(file_)
